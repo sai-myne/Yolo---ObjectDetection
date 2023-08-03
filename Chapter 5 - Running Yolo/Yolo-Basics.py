a6d76bb9d42c1a4f245a8a030335d19a45144dc5
sai-myne <snm.tgi@gmail.com>
2023-08-03 10:56:34 +0800

    Yolo basic, Yolo weight folder added

---
+++ Chapter 5 - Running Yolo/Yolo-Basics.py
@@ -0,0 +1,27 @@
+from ultralytics import YOLO
+
+import cv2
+
+model = YOLO('../Yolo-Weight/yolov8n.pt')
+results = model('Images/3.png', show=True)
+
+cv2.waitKey(0)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
